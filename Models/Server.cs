45a8891743039792a6e3c3b64ab570509894bbc8
Eric VanWagner <Eric.VanWagner@core.ironrodhealth.com>
2019-07-02 14:41:28 -0700

    inital commit

---
+++ Models/Server.cs
@@ -0,0 +1,9 @@
+namespace NexusReportingApi.Models
+{
+    public class Server{
+        public int Id {get;set;}
+        public string Name {get;set;}
+        public bool isHealthy {get;set;}
+        
+    }
+}
